e733cd1780658aefd0ecfe067718ed1e2c8b3ed1
Dariya Sydykova <dariya.k.sydykova@gmail.com>
2015-05-28 13:44:07 -0500

    adding simulated trees and msa

--- src/generate_balanced_tree_ape.R
+++ src/generate_balanced_tree_ape.R
@@ -3,4 +3,5 @@ library(ape)
 n <- 64 # num taxa must be a power of 2 for a binary tree
 tree <- stree(n, type = "balanced") # generated binary tree w/ 64 taxa. There are no branch lengths, though. Can add them by accessing tree$edge.length
 tree$edge.length <- 0.5 # sets all branch lengths to 0.5
-tree$edge.length<-runif(n=**nrow(tree$edge),min=0,max=2) # draws branch lengths from uniform distribution with min0, max2
+tree$edge.length<-runif(n=nrow(tree$edge),min=0,max=2) # draws branch lengths from uniform distribution with min0, max2
+write.tree(tree, file="../test_trees/test1.tre")
