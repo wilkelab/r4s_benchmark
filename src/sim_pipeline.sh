1833b45a5f3a03d2f236f6ec0dfbfa0e88a63983
Daria Sydykova <ds29583@login1.stampede.tacc.utexas.edu>
2015-06-07 10:38:55 -0500

    fixing errors in the pipeline sim_pipeline.sh

--- src/sim_pipeline.sh
+++ src/sim_pipeline.sh
@@ -6,9 +6,13 @@ bl=$2
 tree=test_t${num}_b${bl}.tre
 model=mg
 aln=${model}_seq_t${num}_b${bl}.fasta
-r4s_norm_rates=$r4s_rates_t${num}_b${bl}.txt
-r4s_unnorm_rates=$r4s_rates_t${num}_b${bl}.txt
-r4s_tree=$r4s_rates_t${num}_b${bl}.txt
+r4s_norm_rates=r4s_rates_t${num}_b${bl}.txt
+r4s_unnorm_rates=r4s_rates_t${num}_b${bl}.txt
+r4s_tree=r4s_rates_t${num}_b${bl}.txt
+
+if [ -f "trees/${tree}" ]; then
+	rm trees/${tree}
+fi
 
 if [ ! -d "trees" ]; then
 	mkdir "trees"
@@ -21,6 +25,22 @@ if [ ! -d "aln" ]; then
 	mkdir "aln"
 	mkdir "aln/nuc"
 	mkdir "aln/aa"
+fi
+
+if [ -f "aln/nuc/$aln" ]; then
+	rm aln/nuc/$aln
+fi
+
+if [ -f "sim_site_rates/pyvolve_out/site_rates_t${num}_b${bl}.txt" ]; then
+	rm sim_site_rates/pyvolve_out/site_rates_t${num}_b${bl}.txt
+fi
+
+if [ -f "sim_site_rates/pyvolve_out/site_rates_info_t${num}_b${bl}.txt" ]; then
+	rm sim_site_rates/pyvolve_out/site_rates_t${num}_b${bl}.txt
+fi
+
+if [ -f "sim_site_rates/final_site_rates/site_rates_t${num}_b${bl}.txt" ]; then
+	rm sim_site_rates/final_site_rates/site_rates_t${num}_b${bl}.txt
 fi
 
 ##simulate multiple sequence alignment based on the tree
@@ -44,6 +64,10 @@ fi
 ##merge pyvolve output
 Rscript src/merge_site_rates.r sim_site_rates/pyvolve_out/site_rates_t${num}_b${bl}.txt sim_site_rates/pyvolve_out/site_rates_info_t${num}_b${bl}.txt 
 
+if [ -f "aln/aa/$aln" ]; then
+	rm aln/aa/$aln
+fi
+
 ##convert an alignment from nuc to aa 
 python src/translate_aln.py aln/nuc/$aln
 
@@ -54,7 +78,7 @@ if [ ! -d "r4s_site_rates/" ]; then
 fi
 
 ##run rate4site 
-../rate4site.3.2.source/sourceMar09/rate4site -s aln/aa/$aln -t trees/$tree -o $r4s_norm_rates -y $r4s_unnorm_rates -x $r4s_tree
+echo "../rate4site.3.2.source/sourceMar09/rate4site -s aln/aa/$aln -t trees/$tree -o $r4s_norm_rates -y $r4s_unnorm_rates -x $r4s_tree"
 if [ -f $r4s_norm_rates ]; then
 	mv $r4s_norm_rates r4s_site_rates/final_site_rates/
 	mv $r4s_unnorm_rates r4s_site_rates/r4s_out/
