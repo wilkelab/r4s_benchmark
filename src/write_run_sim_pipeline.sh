ad5e3c1a36c3d8a04b91aff685e2309e5e269458
Dariya Sydykova <dariya.k.sydykova@gmail.com>
2015-07-28 12:50:39 -0500

    updating scripts to rerun ms models

--- src/write_run_sim_pipeline.sh
+++ src/write_run_sim_pipeline.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sim_num=30
-sim_model_arr=(dN dN_dS ms_dS ms_no_dS)
+sim_model_arr=(ms_dS ms_no_dS)
 taxa_num_arr=(32 64 128 256)
-br_len_arr=(0.001 0.0033 0.01 0.033 0.1)
+#br_len_arr=(0.001 0.0033 0.01 0.033 0.1)
+br_len_arr=(0.1 0.33 1.0 3.3)
 
 rm ./src/run_sim_pipeline.sh
 
