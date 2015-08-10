dc44f197aa8f34094e9c7c1a9ac777d57542efc3
Dariya Sydykova <dariya.k.sydykova@gmail.com>
2015-08-10 11:11:57 -0500

    Updating sim output merge scripts

--- src/write_run_merge.sh
+++ src/write_run_merge.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sim_num=30
-sim_model_arr=("dN" "dN_dS" "ms_dS" "ms_no_dS")
+sim_model_arr=("mech_codon_dN" "mech_codon_dN_dS")
 taxa_num_arr=(32 64 128 256)
 br_len_arr=(0.001 0.0033 0.01 0.033 0.1)
 
-if [ -f "./src/run_plots.sh" ]; then
-	rm ./src/run_plots.sh 
+if [ -f "./src/run_merge.sh" ]; then
+	rm ./src/run_merge.sh 
 fi
 
 if [ ! -d "plots" ]; then
