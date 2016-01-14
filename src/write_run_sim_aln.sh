5503561eec6ccefb29d9cb0d50eef16d417c07e5
dariyasydykova <ds29583@login1.ls5.tacc.utexas.edu>
2016-01-14 15:28:47 -0600

    Adding true rates for mechanistic codon model simulations

--- src/write_run_sim_aln.sh
+++ src/write_run_sim_aln.sh
@@ -3,7 +3,6 @@ rep_num=50
 bias_arr=("bias" "nobias")
 taxa_num=11
 br_len_arr=(0.0025 0.01 0.04 0.16 0.64)
-count_line=0
 
 if [ -f "./src/run_sim_aln.sh" ]; then
 	rm ./src/run_sim_aln.sh 
@@ -11,15 +10,16 @@ fi
 
 for bias in ${bias_arr[*]}
 do
-	if [ ! -d "../r4s_benchmark_data/aln/mech_codon/nuc" ]; then
-		mkdir "../r4s_benchmark_data/aln/mech_codon/nuc"
+	if [ ! -d "$SCRATCH/r4s_benchmark_data/aln/mech_codon/nuc" ]; then
+		mkdir "$SCRATCH/r4s_benchmark_data/aln/mech_codon/nuc"
 	fi
 		
-	if [ ! -d "../r4s_benchmark_data/aln/mech_codon/ancestral_aln/" ]; then
-		mkdir "../r4s_benchmark_data/aln/mech_codon/ancestral_aln/"
+	if [ ! -d "$SCRATCH/r4s_benchmark_data/aln/mech_codon/ancestral_aln/" ]; then
+		mkdir "$SCRATCH/r4s_benchmark_data/aln/mech_codon/ancestral_aln/"
 	fi	
 	
-	if [ ! -d "./mech_codon/true_rates" ]; then
+	if [ ! -d "./mech_codon" ]; then
+		mkdir "./mech_codon"
 		mkdir "./mech_codon/true_rates"
 	fi
 	 
@@ -32,11 +32,7 @@ do
 				aln=rep${j}_n${i}_bl${br_len}_${bias}.fasta
 				tree=n${i}_bl${br_len}.tre
 				true_rates=sim_rates_rep${j}_n${i}_bl${br_len}_${bias}.txt
-				echo "python ./src/simulate_mech_codon_aln.py $bias ../r4s_benchmark_data/trees/${tree} ../r4s_benchmark_data/aln/mech_codon/nuc/$aln ./mech_codon/true_rates/${true_rates} &" >> ./src/run_sim_aln.sh
-				count_line=$((count_line+1))
-				if [  $((count_line%10)) -eq 0 ]; then 
-					echo "wait" >> ./src/run_sim_aln.sh 
-				fi
+				echo "python ../../../home1/02159/ds29583/r4s_benchmark/src/simulate_mech_codon_aln.py $bias ./r4s_benchmark_data/trees/${tree} ./r4s_benchmark_data/aln/mech_codon/nuc/$aln ../../../home1/02159/ds29583/r4s_benchmark/mech_codon/true_rates/${true_rates}" >> ./src/run_sim_aln.sh
 			done
 		done
 	done
