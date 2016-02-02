d4c73d166b0d5c14bc99303d86f8b90f32f4fe79
dariyasydykova <ds29583@login1.ls5.tacc.utexas.edu>
2016-02-02 08:07:52 -0600

    Adding updated mutsel model r4s rates

--- src/r4s_pipeline.sh
+++ src/r4s_pipeline.sh
@@ -3,6 +3,7 @@ bias=$1
 br_len=$2
 taxa_num=$3
 rep_num=$4
+model=$5
 
 aln=rep${rep_num}_n${taxa_num}_bl${br_len}_${bias}.fasta
 tree=n${taxa_num}_bl${br_len}.tre
@@ -10,10 +11,10 @@ r4s_norm_rates=r4s_norm_rates_rep${rep_num}_n${taxa_num}_bl${br_len}_${bias}.txt
 r4s_orig_rates=r4s_orig_rates_rep${rep_num}_n${taxa_num}_bl${br_len}_${bias}.txt
 
 ##convert an alignment from nuc to aa 
-python ../../../home1/02159/ds29583/r4s_benchmark/src/translate_aln.py ./r4s_benchmark_data/aln/mech_codon/nuc/${aln} ./r4s_benchmark_data/aln/mech_codon/aa/${aln}
+python ../../../home1/02159/ds29583/r4s_benchmark/src/translate_aln.py ./r4s_benchmark_data/aln/${model}/nuc/${aln} ./r4s_benchmark_data/aln/${model}/aa/${aln}
 
 ##run rate4site 
-../../../home1/02159/ds29583/rate4site.3.2.source/sourceMar09/rate4site -s ./r4s_benchmark_data/aln/mech_codon/aa/${aln} -t ./r4s_benchmark_data/trees/${tree} -o ../../../home1/02159/ds29583/r4s_benchmark/mech_codon/r4s_rates/raw_rates/${r4s_norm_rates} -y ../../../home1/02159/ds29583/r4s_benchmark/mech_codon/r4s_rates/raw_rates/${r4s_orig_rates} 
+../../../home1/02159/ds29583/rate4site.3.2.source/sourceMar09/rate4site -s ./r4s_benchmark_data/aln/${model}/aa/${aln} -t ./r4s_benchmark_data/trees/${tree} -o ../../../home1/02159/ds29583/r4s_benchmark/${model}/r4s_rates/raw_rates/${r4s_norm_rates} -y ../../../home1/02159/ds29583/r4s_benchmark/${model}/r4s_rates/raw_rates/${r4s_orig_rates} 
 if [ -f r4s.res ]; then
 	rm r4s.res 
 fi 
