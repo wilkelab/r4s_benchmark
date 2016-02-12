a2bb57d75384e1c4d67e8d753fc17828231236ec
dariyasydykova <ds29583@login2.ls5.tacc.utexas.edu>
2016-02-12 05:26:07 -0600

    Adding new simulated rates. Both assigned rates and mathematically inferred rates.

--- src/write_run_sim_aln.sh
+++ src/write_run_sim_aln.sh
@@ -47,7 +47,7 @@ do
 				sim_rates=sim_rates_rep${j}_n${i}_bl${br_len}_${bias}.txt
 				sim_rates_info=sim_rates_info_rep${j}_n${i}_bl${br_len}_${bias}.txt
 				true_rates=true_rates_rep${j}_n${i}_bl${br_len}_${bias}.txt
-				echo "python ../../../home1/02159/ds29583/r4s_benchmark/src/simulate_mech_codon_aln.py $bias ./r4s_benchmark_data/trees/${tree} ./r4s_benchmark_data/aln/mech_codon/nuc/${aln} ../../../home1/02159/ds29583/mech_codon/sim_rates/assigned_rates/raw_rates/${sim_rates} ../../../home1/02159/ds29583/mech_codon/sim_rates/assigned_rates/raw_rates/${sim_rates_info} ../../../home1/02159/ds29583/r4s_benchmark/mech_codon/sim_rates/true_rates/${true_rates}"  >> ./src/run_sim_aln.sh
+				echo "python ../../../home1/02159/ds29583/r4s_benchmark/src/simulate_mech_codon_aln.py $bias ./r4s_benchmark_data/trees/${tree} ./r4s_benchmark_data/aln/mech_codon/nuc/${aln} ../../../home1/02159/ds29583/r4s_benchmark/mech_codon/sim_rates/assigned_rates/raw_rates/${sim_rates} ../../../home1/02159/ds29583/r4s_benchmark/mech_codon/sim_rates/assigned_rates/raw_rates/${sim_rates_info} ../../../home1/02159/ds29583/r4s_benchmark/mech_codon/sim_rates/true_rates/${true_rates}"  >> ./src/run_sim_aln.sh
 			done
 		done
 	done
