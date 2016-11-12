28c8b34db1fd01b8b52154f0caca8032e4741e61
dariyasydykova <dariya.k.sydykova@gmail.com>
2016-11-11 21:47:30 -0600

    Adding new trees based on new alignments

--- src/write_run_raxml.sh
+++ src/write_run_raxml.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-hiv_aln_files=$HOME/r4s_benchmark/natural_prot/aln/hiv1/*_clean_dna.fasta
+hiv_aln_files=$HOME/r4s_benchmark/natural_prot/aln/hiv1/*_clean_dna_aligned.fasta
 gpcr_aln_files=$HOME/r4s_benchmark/natural_prot/aln/gpcr/*_n_aligned.fasta
 nuc_aln_files=( ${hiv_aln_files[@]} ${gpcr_aln_files[@]} )
 
@@ -9,9 +9,21 @@ rm ./src/run_raxml.sh
 for aln in ${nuc_aln_files[*]}
 do
 	prot_name=`echo $aln | grep -oP 'hiv1_[a-zA-Z0-9]+|ENST[0-9]+'`
-	out_tree=${prot_name}.tre
+	out_tree=${prot_name}_n.tre
 	out_dir=~/r4s_benchmark/natural_prot/trees/
 	echo $HOME/standard-RAxML/raxmlHPC-PTHREADS-SSE3 -T 48 -s $aln -w $out_dir -n $out_tree -m GTRGAMMA -p 1 >> ./src/run_raxml.sh
+done
+
+hiv_aln_files=$HOME/r4s_benchmark/natural_prot/aln/hiv1/*_clean_protein.fasta
+gpcr_aln_files=$HOME/r4s_benchmark/natural_prot/aln/gpcr/*_p_aligned.fasta
+prot_aln_files=( ${hiv_aln_files[@]} ${gpcr_aln_files[@]} )
+
+for aln in ${prot_aln_files[*]}
+do
+	prot_name=`echo $aln | grep -oP 'hiv1_[a-zA-Z0-9]+|ENST[0-9]+'`
+	out_tree=${prot_name}_p.tre
+	out_dir=~/r4s_benchmark/natural_prot/trees/
+	echo $HOME/standard-RAxML/raxmlHPC-PTHREADS-SSE3 -T 48 -s $aln -w $out_dir -n $out_tree -m PROTCATLG -p 1 >> ./src/run_raxml.sh
 done
 
 chmod +x ./src/run_raxml.sh
