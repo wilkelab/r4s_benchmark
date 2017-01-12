71d736a37160cee3bd99a25a1417bdd59b769a40
Dariya Sydykova <dariya.k.sydykova@gmail.com>
2017-01-12 10:44:56 -0600

    adding new alignment file structure

--- src/run_translate_aln_aa_to_codon.sh
+++ src/run_translate_aln_aa_to_codon.sh
@@ -1,20 +1,18 @@
 #! /bin/bash
 
-hiv_aln_files=natural_prot/aln/hiv1/*_clean_protein.fasta
-gpcr_aln_files=natural_prot/aln/gpcr/*_p_aligned.fasta
+hiv_aln_files=natural_prot/aln/aligned_seqs/*_clean_protein.fasta
+gpcr_aln_files=natural_prot/aln/aligned_seqs/*_p.fasta
 
 for aln in ${gpcr_aln_files[*]}
 do
 	prot_name=`echo $aln | grep -o -E 'ENST[0-9]+'`
-	unaln_codon_file=${prot_name}_n.fasta
-	out_aln=${prot_name}_n_aligned.fasta
-	python src/translate_aln_aa_to_codon.py $aln natural_prot/aln/gpcr/$unaln_codon_file natural_prot/aln/gpcr/$out_aln
+	codon_file=${prot_name}_n.fasta
+	python src/translate_aln_aa_to_codon.py $aln natural_prot/aln/raw_aln/$codon_file natural_prot/aln/back_translated_aln/$codon_file
 done
 
 for aln in ${hiv_aln_files[*]}
 do
 	prot_name=`echo $aln | grep -o -E 'hiv1_[a-zA-Z0-9]+'`
-	unaln_codon_file=${prot_name}_clean_dna.fasta
-	out_aln=${prot_name}_clean_dna_aligned.fasta
-	python src/translate_aln_aa_to_codon.py $aln natural_prot/aln/hiv1/$unaln_codon_file natural_prot/aln/hiv1/$out_aln
+	codon_file=${prot_name}_clean_dna.fasta
+	python src/translate_aln_aa_to_codon.py $aln natural_prot/aln/raw_aln/$codon_file natural_prot/aln/back_translated_aln/$codon_file
 done
--- src/write_run_align_natural_prot.sh
+++ src/write_run_align_natural_prot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gpcr_aln_files=./natural_prot/aln/r4s_benchmark/natural_prot/aln/raw_aln/*_p.fasta
+aln_files=./natural_prot/aln/raw_aln/*_p.fasta
 
 if [ -f ./src/align_natural_prot.sh ]; then
 	rm ./src/align_natural_prot.sh  
@@ -9,8 +9,7 @@ for aln in ${aln_files[*]}
 do
 	out_dir=aligned_seqs
 	out_file="${aln/raw_aln/$out_dir}"
-	echo $out_file
-	#echo mafft $aln \> $aligned_n_file >> ./src/align_natural_prot.sh
+	echo mafft $aln \> $out_file >> ./src/align_natural_prot.sh
 done
 
 chmod +x ./src/align_natural_prot.sh
