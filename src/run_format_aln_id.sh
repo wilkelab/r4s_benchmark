cf0e114a1439270d5713aa67b58f3aa0ef5bc3ed
dariyasydykova <dariya.k.sydykova@gmail.com>
2017-01-11 21:30:57 -0600

    Adding data for one more natural protein

--- src/run_format_aln_id.sh
+++ src/run_format_aln_id.sh
@@ -7,7 +7,7 @@ nuc_aln_files=( ${hiv_aln_files[@]} ${gpcr_aln_files[@]} )
 for aln in ${nuc_aln_files[*]}
 do
 	prot_name=`echo $aln | grep -oP 'hiv1_[a-zA-Z0-9]+|ENST[0-9]+'`
-	out_aln=$HOME/r4s_benchmark/natural_prot/aln/hiv1/${prot_name}_clean_dna_reformatted.fasta
+	out_aln=$HOME/r4s_benchmark/natural_prot/aln/processed_aln/${prot_name}_clean_dna_reformatted.fasta
 	echo $out_aln
 	python src/format_aln_id.py $aln $out_aln
 done
--- src/write_run_raxml.sh
+++ src/write_run_raxml.sh
@@ -8,25 +8,31 @@ for aln in ${nuc_aln_files[*]}
 do
 	prot_name=`echo $aln | grep -oP 'hiv1_[a-zA-Z0-9]+|ENST[0-9]+'`
 	out_tree=${prot_name}_n.tre
-	out_dir=~/r4s_benchmark/natural_prot/trees/
+	out_dir=~/r4s_benchmark/natural_prot/trees
 	
-	if [ -f {$out_dir}/${out_tree} ]; then
+	if [ -f ${out_dir}/RAxML_bestTree.${out_tree} ]; then
+		echo ${out_dir}/RAxML_bestTree.${out_tree}
 		continue 
 	else
 		echo $HOME/standard-RAxML/raxmlHPC-PTHREADS-SSE3 -T 48 -s $aln -w $out_dir -n $out_tree -m GTRGAMMA -p 1 >> ./src/run_raxml.sh
 	fi
 done
 
-# hiv_aln_files=$HOME/r4s_benchmark/natural_prot/aln/hiv1/*_clean_protein.fasta
-# gpcr_aln_files=$HOME/r4s_benchmark/natural_prot/aln/gpcr/*_p_aligned.fasta
-# prot_aln_files=( ${hiv_aln_files[@]} ${gpcr_aln_files[@]} )
-# 
-# for aln in ${prot_aln_files[*]}
-# do
-# 	prot_name=`echo $aln | grep -oP 'hiv1_[a-zA-Z0-9]+|ENST[0-9]+'`
-# 	out_tree=${prot_name}_p.tre
-# 	out_dir=~/r4s_benchmark/natural_prot/trees/
-# 	echo $HOME/standard-RAxML/raxmlHPC-PTHREADS-SSE3 -T 48 -s $aln -w $out_dir -n $out_tree -m PROTCATLG -p 1 >> ./src/run_raxml.sh
-# done
-# 
-# chmod +x ./src/run_raxml.sh
+hiv_aln_files=$HOME/r4s_benchmark/natural_prot/aln/hiv1/*_clean_protein_reformatted.fasta
+gpcr_aln_files=$HOME/r4s_benchmark/natural_prot/aln/gpcr/*_p_aligned.fasta
+prot_aln_files=( ${hiv_aln_files[@]} ${gpcr_aln_files[@]} )
+
+for aln in ${prot_aln_files[*]}
+do
+	prot_name=`echo $aln | grep -oP 'hiv1_[a-zA-Z0-9]+|ENST[0-9]+'`
+	out_tree=${prot_name}_p.tre
+	out_dir=~/r4s_benchmark/natural_prot/trees
+	if [ -f ${out_dir}/RAxML_bestTree.${out_tree} ]; then
+		echo ${out_dir}/RAxML_bestTree.${out_tree}
+		continue 
+	else
+		echo $HOME/standard-RAxML/raxmlHPC-PTHREADS-SSE3 -T 48 -s $aln -w $out_dir -n $out_tree -m PROTCATLG -p 1 >> ./src/run_raxml.sh
+	fi
+done
+
+chmod +x ./src/run_raxml.sh
