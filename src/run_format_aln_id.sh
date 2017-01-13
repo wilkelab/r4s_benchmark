a7d09d742ba4a0059f626aecf928748e5502b734
dariyasydykova <dariya.k.sydykova@gmail.com>
2017-01-13 10:42:25 -0600

    adding reformatted aln and tree files

--- src/run_format_aln_id.sh
+++ src/run_format_aln_id.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-hiv_aln_files=$HOME/r4s_benchmark/natural_prot/aln/hiv1/*_clean_dna_aligned.fasta
-gpcr_aln_files=$HOME/r4s_benchmark/natural_prot/aln/gpcr/*_n_aligned.fasta
-nuc_aln_files=( ${hiv_aln_files[@]} ${gpcr_aln_files[@]} )
+nuc_aln_files=$HOME/r4s_benchmark/natural_prot/aln/back_translated_aln/*.fasta
 
 for aln in ${nuc_aln_files[*]}
 do
 	prot_name=`echo $aln | grep -oP 'hiv1_[a-zA-Z0-9]+|ENST[0-9]+'`
-	out_aln=$HOME/r4s_benchmark/natural_prot/aln/processed_aln/${prot_name}_clean_dna_reformatted.fasta
-	echo $out_aln
-	python src/format_aln_id.py $aln $out_aln
+	out_file=$HOME/r4s_benchmark/natural_prot/aln/reformatted_aln/${prot_name}.fasta
+	python src/format_aln_id.py $aln $out_file
 done
--- src/run_format_tree_id.sh
+++ src/run_format_tree_id.sh
@@ -4,8 +4,7 @@ tree_files=$HOME/r4s_benchmark/natural_prot/trees/RAxML_bestTree.*.tre
 
 for tree in ${tree_files[*]}
 do
-	prot_name=`echo $tree | grep -oP '(hiv1_[a-zA-Z0-9]+|ENST[0-9]+)_[np]'`
-	out_tree=$HOME/r4s_benchmark/natural_prot/trees/RAxML_bestTree.${prot_name}_reformatted.tre
-	echo $out_tree
+	prot_name=`echo $tree | grep -oP '(hiv1_[a-zA-Z0-9]+|ENST[0-9]+)'`
+	out_tree=$HOME/r4s_benchmark/natural_prot/trees/reformatted_trees/${prot_name}.tre
 	python src/format_tree_id.py $tree $out_tree
 done
