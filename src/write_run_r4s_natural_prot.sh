9ec3f7dab1e6a8dd2dab48cf4dcb34beee4cda89
dariyasydykova <dariya.k.sydykova@gmail.com>
2017-01-15 09:13:59 -0600

    Adding inferred dNdS and r4s rates files

--- src/write_run_r4s_natural_prot.sh
+++ src/write_run_r4s_natural_prot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-hiv_aln_files=$HOME/r4s_benchmark/natural_prot/aln/hiv1/*_clean_protein.fasta
-gpcr_aln_files=$HOME/r4s_benchmark/natural_prot/aln/gpcr/*_p_aligned.fasta
+hiv_aln_files=$HOME/r4s_benchmark/natural_prot/aln/aligned_seqs/*_clean_protein.fasta
+gpcr_aln_files=$HOME/r4s_benchmark/natural_prot/aln/aligned_seqs/*_p.fasta
 prot_aln_files=( ${hiv_aln_files[@]} ${gpcr_aln_files[@]} )
 
 if [ -f "$SCRATCH/run_r4s_natural_prot.sh" ]; then
@@ -10,9 +10,9 @@ fi
 for aln in ${prot_aln_files[*]}
 do	
 	prot_name=`echo $aln | grep -oP 'hiv1_[a-zA-Z0-9]+|ENST[0-9]+'`
-	r4s_norm_rates=$HOME/r4s_benchmark/natural_prot/r4s_rates/raw_rates/${prot_name}_norm_rates.txt
-	r4s_orig_rates=$HOME/r4s_benchmark/natural_prot/r4s_rates/raw_rates/${prot_name}_orig_rates.txt
-	tree_file=$HOME/r4s_benchmark/natural_prot/trees/RAxML_bestTree.${prot_name}_p.tre
+	r4s_norm_rates=$HOME/r4s_benchmark/natural_prot/r4s_rates/${prot_name}_norm_rates.txt
+	r4s_orig_rates=$HOME/r4s_benchmark/natural_prot/r4s_rates/${prot_name}_orig_rates.txt
+	tree_file=$HOME/r4s_benchmark/natural_prot/trees/RAxML_bestTree.${prot_name}.tre
 	echo $HOME/rate4site.3.2.source/sourceMar09/rate4site -s $aln -t $tree_file -o $r4s_norm_rates -y $r4s_orig_rates >> $SCRATCH/run_r4s_natural_prot.sh
 done
 
